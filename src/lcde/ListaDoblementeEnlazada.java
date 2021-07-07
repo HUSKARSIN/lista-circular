62aa260c13fc818a21304659ed0377e8d4557553
HUSKARSIN <mishirigami@gmail.com>
2021-07-07 05:12:22 -0400

    lista circular

---
+++ src/lcde/ListaDoblementeEnlazada.java
@@ -0,0 +1,4 @@
+package lcde;
+
+public class ListaDoblementeEnlazada {
+}
---
+++ src/lcde/Nodo.java
@@ -0,0 +1,7 @@
+package lcde;
+
+public class Nodo {
+    private int valor;
+    private Nodo siguiente;
+    private Nodo anterios
+}
